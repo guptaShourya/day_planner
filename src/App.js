b800a9722393a09dfc0af07fe531f761733681f1
Shourya Gupta <apid.shourya@gmail.com>
2021-09-24 16:03:37 +0530

    Update for Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import LandingPage from './LandingPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import RegisterMain from './Register';
 import './App.css';
 
 function App() {
   return (
-    <LandingPage/>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
+        <Route exact path='/login'>
+          <RegisterMain type={true} />
+        </Route>
+        <Route exact path='/signup'>
+          <RegisterMain type={false} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/LandingPage/NavBar/ItemsRight.js
+++ src/LandingPage/NavBar/ItemsRight.js
@@ -2,25 +2,30 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 
 const ItemsRight = () => {
 
     return (
-        <Grid item className = "Lp_items">
+        <Grid item className="Lp_items">
             <Grid container direction="row" justifyContent="flex-end" alignItems="center">
                 <Grid item>
-                    <Button>
-                        <Typography variant="h6" className="Lp_navbar_text">
-                            Sign In
-                        </Typography>
-                    </Button>
+                    <Link to='/login'>
+                        <Button id="Lp_signin_button">
+                            <Typography variant="h6" className="Lp_navbar_text">
+                                Sign In
+                            </Typography>
+                        </Button>
+                    </Link>
                 </Grid>
-                <Grid item id = "Lp_signup_container">
-                    <Button id = "Lp_signup_button">
-                        <Typography variant="h6" className="Lp_navbar_text" style={{ color: "white" }}>
-                            Sign Up
-                        </Typography>
-                    </Button>
+                <Grid item id="Lp_signup_container">
+                    <Link to='/signup'>
+                        <Button id="Lp_signup_button">
+                            <Typography variant="h6" className="Lp_navbar_text" style={{ color: "white" }}>
+                                Sign Up
+                            </Typography>
+                        </Button>
+                    </Link>
                 </Grid>
             </Grid>
         </Grid>
